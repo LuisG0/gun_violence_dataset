ed822a7d50a940b36086d3eed67a91b697fcbeae
Luis <luisgallego778@gmail.com>
2022-03-14 23:31:41 +0100

    Incidents per pop

--- mapa_states_Luis.py
+++ mapa_states_Luis.py
@@ -1,7 +1,13 @@
+from matplotlib.pyplot import text
 import pandas as pd
 
 incidents = pd.read_csv('gun_violence_dataset/incidents_dataset.csv')
 incidents = incidents.drop(labels=['Unnamed: 0'], axis=1,errors='ignore')
+
+population = pd.read_csv('data/nst-est2019-alldata.csv')
+population = population[["NAME","POPESTIMATE2017"]].rename(columns={"NAME":"state","POPESTIMATE2017":"pop"})
+
+
 import datetime
 incidents["month"] = incidents["date"].apply(lambda date: datetime.datetime.strptime(date, "%Y-%m-%d").month)
 incidents["year"] = incidents["date"].apply(lambda date: datetime.datetime.strptime(date, "%Y-%m-%d").year)
@@ -22,25 +28,32 @@ def state2code(df):
     return df
 
 incidents = state2code(incidents)
-#incidents = incidents.query("year == 2014 & month == 1")
 
 incidents_per_state = incidents.groupby(["code","state","month/year"], as_index=False)
 incidents_map = incidents_per_state["n_injured"].agg(['sum','count']).rename(columns={'count':'n_incidents','sum':'n_injured'})
 incidents_map["n_killed"] = incidents_per_state["n_killed"].agg(['sum']).rename(columns={'sum':'n_killed'})["n_killed"]
 incidents_map = incidents_map.reset_index()
 
+# Get incidents per million people
+incidents_map = incidents_map.join(population.set_index('state'),on='state')
+incidents_map["injuried_per_1M"] = incidents_map["n_incidents"] / incidents_map["pop"] * 1000000
+incidents_map
+
 def plot_map(df, col, pal):
     df = df[df["n_incidents"]>0]
     fig = px.choropleth(df, locations="code", locationmode='USA-states', 
-                  color=col, hover_name="state", 
-                  title=col, hover_data=["n_injured","n_killed","n_incidents"], 
+                  color=col,
+                  hover_name="state", 
+                  hover_data=["n_injured","n_killed","n_incidents"], 
                   color_continuous_scale=pal,
                   animation_frame="month/year",
                   projection="natural earth",
-                  scope="north america")
+                  scope="north america",
+                  title="Heridos con armas por cada millón de habitantes"
+                  )
     fig.update_coloraxes(cmax =df[col].max(),cmin =df[col].min())
     fig.show()
 
 
-plot_map(incidents_map, 'n_injured', 'Reds') #n_injured, n_killed or n_incidents
+plot_map(incidents_map, 'injuried_per_1M', 'Reds') #n_injured, n_killed or n_incidents
 
